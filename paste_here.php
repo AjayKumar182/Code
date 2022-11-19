c19ae2456c13a0e12242a14aa88ae237421d8856
AjayKumar182 <105137351+AjayKumar182@users.noreply.github.com>
2022-11-19 08:01:50 +0530

    Add files via upload

---
+++ paste_here.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>mini paste bin</title>
+  <link rel = "stylesheet" type = "text/css" href = "style.css">   
+</head>
+<body>
+<div id=frm2>
+<form  action = "paste_data.php"  method = "POST">  
+<center> paste/enter your text here</center>
+<textarea rows="30" cols="60"  placeholder="enter your text here"  name="data"></textarea>
+   <input type =  "submit" id = "btn3" value = "save"  />
+  <!--<button type="button" onclick="document.location='logout.php' ">Logout</button>-->
+</form>
+</div>
+</body>
+</htm>
